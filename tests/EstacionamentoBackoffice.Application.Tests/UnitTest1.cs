27f4a1a70cf41f1f3993bd2a581184b8600774bb
Igor Shmarov <igor_memories@hotmail.com>
2023-11-17 19:42:14 -0300

    Adicionado Projetos de Testes de Unidade

---
+++ tests/EstacionamentoBackoffice.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EstacionamentoBackoffice.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/EstacionamentoBackoffice.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EstacionamentoBackoffice.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
